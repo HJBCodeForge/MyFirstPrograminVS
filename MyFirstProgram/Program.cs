1d08b4de18e9a9b80aba56b0df993315122066fc
owner <owner@LAPTOP-H1VSQQKE>
2025-10-03 12:28:35 -0400

    Add project files.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.Write("Hey!");
+//Console.WriteLine("Hello!");
+
+////This is a comment
+///*This is a
+// * Multi line comment
+// */
+//Console.WriteLine("\tDude"); //Example of escape sequence
+
+//Console.ReadKey(); //Keeps the console window open
+
+//Creating variables, 2 steps: Declaration and Initialization
+
+//Data types: int, double, char, string, bool
+int x; //Declaration
+x = 123; //Initialization, can only store whole integers
+
+int age = 21; //Declaration + Initialization
+double pi = 3.14; //Can store decimal numbers
+bool isCSharpFun = true; //Can only store true or false
+char grade = 'A'; //Can only store a single character, must use single quotes
+string name = "John Doe"; //Can store multiple characters, must use double quotes
+
+Console.WriteLine(name + " age " +age + " recieved a grade of " + grade);
+
+Console.ReadKey();
