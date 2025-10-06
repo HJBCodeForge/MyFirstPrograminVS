bfdbb5c4a238482b04049b72a53a49255c9b687e
HJBCodeForge <henning@hjbcodeforge.com>
2025-10-06 11:07:31 -0400

    11. LOGICAL OPERATORS

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -175,6 +175,23 @@
 //        break;
 //}
 
+//11. LOGICAL OPERATORS = used to determine the logic between variables or values
+// && (AND), || (OR), ! (NOT)
+
+//Console.WriteLine("What's the temperature outside: (C)");
+//double temp = Convert.ToDouble(Console.ReadLine());
+
+//if (temp >= 10 && temp <= 25)
+//{
+//    Console.WriteLine("It's warm outside");
+//}
+//else if (temp <= -50 || temp >= 50)
+//{
+//    Console.WriteLine("Do not go outside!");
+//} else
+//{
+//    Console.WriteLine("The temp is reasonable");
+//}
 
 
 
