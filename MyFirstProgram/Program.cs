695975f31cc4cb25940a1883a82ec6771c154f10
HJBCodeForge <henning@hjbcodeforge.com>
2025-10-06 07:33:28 -0400

    type casting practices

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -13,15 +13,29 @@
 //Creating variables, 2 steps: Declaration and Initialization
 
 //Data types: int, double, char, string, bool
-int x; //Declaration
-x = 123; //Initialization, can only store whole integers
+//int x; //Declaration
+//x = 123; //Initialization, can only store whole integers
 
-int age = 21; //Declaration + Initialization
-double pi = 3.14; //Can store decimal numbers
-bool isCSharpFun = true; //Can only store true or false
-char grade = 'A'; //Can only store a single character, must use single quotes
-string name = "John Doe"; //Can store multiple characters, must use double quotes
+//int age = 21; //Declaration + Initialization
+//double pi = 3.14; //Can store decimal numbers
+//bool isCSharpFun = true; //Can only store true or false
+//char grade = 'A'; //Can only store a single character, must use single quotes
+//string name = "John Doe"; //Can store multiple characters, must use double quotes
 
-Console.WriteLine(name + " age " +age + " recieved a grade of " + grade);
+//Console.WriteLine(name + " age " +age + " recieved a grade of " + grade);
+
+//const double gravity = 9.81; //Constant variable, cannot be changed
+
+//type casting = converting a variable from one data type to another
+//              useful when we accept user input (string)
+
+//double a = 3.14;
+//int b = Convert.ToInt32(a);
+
+//String i = "true";
+//bool j = Convert.ToBoolean(i);
+
+//Console.WriteLine(b.GetType());
+//Console.WriteLine(j.GetType());
 
 Console.ReadKey();
